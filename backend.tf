79d7640fe7af5b7e36f4f62f1dec9476e4c99edb
Taiwo <taiwolawal360@gmail.com>
2023-06-15 15:46:24 +0100

    vpc setup

--- backend.tf
+++ backend.tf
@@ -3,8 +3,7 @@ terraform {
     bucket         = "s3-eks-proj"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
-
+ 
     # For State Locking
     dynamodb_table = "dev-eks-dynamo"
     
