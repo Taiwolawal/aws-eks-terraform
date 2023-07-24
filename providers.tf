8ba2a6abcc2a067e87251c7f6b131b50e231a94e
Taiwo <taiwolawal360@gmail.com>
2023-07-24 13:37:22 +0100

    eks setup finished

--- providers.tf
+++ providers.tf
@@ -16,7 +16,8 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
   }
 }
+
